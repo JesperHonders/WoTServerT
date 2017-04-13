1188ff1f28eca90119efe2b0067a27bf9178ec90
Jesper Honders <Jesper@activecollective.nl>
2017-04-13 11:15:28 +0200

    Posts color to FF28 and 8D4B

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 var express = require('express')
 var app = express()
+var request = require('request')
+
+function getTestPersonaLoginCredentials(callback) {
+  request('https://oege.ie.hva.nl/~palr001/icu/api.php?t=sqi&d=FF28&td=8D4B&c=00FFDF')
+}
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  getTestPersonaLoginCredentials()
+  res.send("send Post")
 })
 
 app.listen(3000, function () {
