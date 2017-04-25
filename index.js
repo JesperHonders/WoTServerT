8ec56cd2c242069bdb0e55e9a08c76b11ff326f2
Jesper Honders <Jesper@activecollective.nl>
2017-04-25 11:24:34 +0200

    Added data route to server

--- index.js
+++ index.js
@@ -2,19 +2,20 @@ var express = require('express')
 var app = express()
 var request = require('request')
 var path = require('path');
+var fs = require('fs')
 
 var heleenSlackURL = 'https://hooks.slack.com/services/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';
 var generalSlackURL = 'https://hooks.slack.com/services/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';
 var newsSlackURL = 'https://hooks.slack.com/services/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';
 var triviaSlackURL = 'https://hooks.slack.com/services/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';
-var newsApiKey = '9e5dce43547a4fe0a8b0b875111ff3c2'; 
+var newsApiKey = '9e5dce43547a4fe0a8b0b875111ff3c2';
 var chipIDs = ['AF3E', '8d4b', 'FF28'];
 
 function setLEDColor(importance) {
     request('https://oege.ie.hva.nl/~palr001/icu/api.php?t=sdc&d=' + chipIDs[0] + '&td=' + chipIDs[0] + '&c=' + importance, function () {
         request('https://oege.ie.hva.nl/~palr001/icu/api.php?t=sqi&d=' + chipIDs[0]);
     });
-    
+
     request('https://oege.ie.hva.nl/~palr001/icu/api.php?t=sdc&d=' + chipIDs[1] + '&td=' + chipIDs[1] + '&c=' + importance, function () {
         request('https://oege.ie.hva.nl/~palr001/icu/api.php?t=sqi&d=' + chipIDs[1]);
     });
@@ -36,27 +37,27 @@ function getNewsArticles(source, sort, amount) {
         }
     });
 }
-//    
+//
 //function getTriviaQuestion(difficulty){
-//    
+//
 //    request('https://opentdb.com/api.php?amount=1&difficulty=' + difficulty, function (error, response, data) {
-//        
+//
 //        var parsedData = JSON.parse(data);
 //        var question = parsedData.results[0].question;
 //        var answer = parsedData.results[0].correct_answer;
 //        setLEDColor('FF0000');
 //        sendSlackMessage(question, triviaSlackURL);
-//        
+//
 //        clearInterval(checkSensorValue);
-//        
+//
 //        setTimeout(function(){
 //            startSensorValueCheck();
 //            sendSlackMessage(answer, triviaSlackURL);
 //        }, 15000);
 //    });
-//} 
-//    
-//getTriviaQuestion('easy');    
+//}
+//
+//getTriviaQuestion('easy');
 //google-news - top
 //bbc-news  - top
 //the-next-web - latest
@@ -79,11 +80,11 @@ function getSensorValues() {
                 value: parsedData.feeds[0].field1,
                 created_at: parsedData.feeds[0].created_at
             }
-            
+
             sensorValue = data;
         }
     });
-   
+
 }
 
 
@@ -145,23 +146,36 @@ function sendSlackMessage(message, url) {
 }
 app.use(express.static(__dirname + '/public'));
 
+
+
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/public/form.html'));
+})
+
+app.get('/pushdata/:distance/:chipId', function (req, res){
+  var distance = req.params.distance;
+  var chipId = req.params.chipId;
+  fs.readFile('./sensordata.json', function (err, data) {
+      if(err) {
+        console.log(err);
+      }
+
+      var json = JSON.parse(data)
+      json.push({distance: distance, chipId : chipId })
+
+      fs.writeFile("./sensordata.json", JSON.stringify(json))
+  })
 })
 
 
 app.get('/message', function (req, res) {
     var message = req.query.text;
     var importance = req.query.importance;
-    
+
     sendSlackMessage(message, generalSlackURL);
     setLEDColor(importance);
 });
 
-app.listen(3000, function () {
+app.listen(3000, '0.0.0.0' ,function () {
     console.log('Example app listening on port 3000!')
 })
-
-
-
-
