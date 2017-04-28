f1c0e98e0adb4e935212e6482303980e645075a9
Jesper Honders <Jesper@activecollective.nl>
2017-04-28 12:01:01 +0200

    Changed port back to 80

--- server.js
+++ server.js
@@ -120,8 +120,8 @@ app.get('/clearJson' , function (req,res){
     })
 })
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!')
+app.listen(80, function () {
+    console.log('Running Server!')
 })
 
 function checkArticle(data) {
